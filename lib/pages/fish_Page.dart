fd0f08f28cb30ab6530a26d1da8c1c37481a1951
Jomer Dorego <jdorego06@gmial.com>
2023-12-03 14:55:17 +0800

    add paginatin in fish page

--- lib/pages/fish_Page.dart
+++ lib/pages/fish_Page.dart
@@ -1,12 +1,8 @@
-// import 'package:fishdroid/pages/home.dart';
-//import 'package:fishdroid/pages/view_fish_page.dart';
-// import 'package:fishdroid/pages/view_fish_page.dart';
-// import 'package:fishdroid/pages/search_page.dart';
-import 'package:fishdroid/services/remote_services.dart';
-import 'package:fishdroid/widgets/fish_item_builder.dart';
-// import 'package:fishdroid/widgets/fish_item_builder.dart';
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_is_empty
 
+import '../services/remote_services.dart';
+import 'package:fishdroid/widgets/fish_item_builder.dart';
+import 'package:flutter/material.dart';
 import '../data/fishes.dart';
 
 class FishPages extends StatefulWidget {
@@ -24,21 +20,40 @@ class _FishPagesState extends State<FishPages> {
   TextEditingController searchController = TextEditingController();
   var header1 = 'Featured';
   var header2 = 'Fishes';
+  var page = 0;
+  var fishCount = 0;
+  // final ScrollController _scrollController = ScrollController();
+  final ScrollController _controller = ScrollController();
+
+  void _scrollToTop() {
+    _controller.animateTo(
+      0.0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.ease,
+    );
+  }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getFishData();
+    getFishCount();
   }
 
   getFishData() async {
-    fishesList = await RemoteService().getFishes();
+    fishesList = await RemoteService().fishPagination(page);
     if (fishesList != null) {
       setState(() {
         isLoaded = true;
       });
     }
+  }
+
+  getFishCount() async {
+    var fs = await RemoteService().rowCount();
+    setState(() {
+      fishCount = fs;
+    });
   }
 
   getSearch(input) async {
@@ -53,7 +68,6 @@ class _FishPagesState extends State<FishPages> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     searchController.dispose();
     super.dispose();
   }
@@ -84,7 +98,7 @@ class _FishPagesState extends State<FishPages> {
                 ),
               ),
               child: Padding(
-                padding: EdgeInsets.all(25.0),
+                padding: const EdgeInsets.all(25.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -147,37 +161,88 @@ class _FishPagesState extends State<FishPages> {
             Container(
               transform: Matrix4.translationValues(0, -120, 0),
               child: Padding(
-                  padding: const EdgeInsets.only(
-                    right: 40,
-                    left: 40,
-                  ),
-                  child: fishesList?.length != 0
-                      ? ListView.builder(
-                          shrinkWrap:
-                              true, // Adjusts the height based on the content
-                          itemCount: fishesList?.length ??
-                              0, // Replace with the actual number of items in your list
-                          itemBuilder: (BuildContext context, int index) {
-                            final fish = fishesList![index];
-                            return FishListItem(fish);
-                          },
-                        )
-                      : Container(
-                          color: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 30,
-                            vertical: 5,
+                padding: const EdgeInsets.only(
+                  right: 40,
+                  left: 40,
+                ),
+                child: fishesList?.length != 0
+                    ? ListView.builder(
+                        controller: _controller,
+                        shrinkWrap:
+                            true, // Adjusts the height based on the content
+                        itemCount: fishesList?.length ??
+                            0, // Replace with the actual number of items in your list
+                        itemBuilder: (BuildContext context, int index) {
+                          final fish = fishesList![index];
+                          return FishListItem(fish);
+                        },
+                      )
+                    : Container(
+                        color: Colors.white,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 5,
+                        ),
+                        child: const Text(
+                          'No Fish Found',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff154670),
                           ),
-                          child: const Text(
-                            'No Fish Found',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff154670),
-                            ),
-                          ),
-                        )),
+                        ),
+                      ),
+              ),
             ),
+            Container(
+              transform: Matrix4.translationValues(0, -120, 0),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    onPressed: page > 0
+                        ? () {
+                            setState(() {
+                              page--;
+                              getFishData();
+                            });
+                            _scrollToTop();
+                          }
+                        : null,
+                    child: Text(
+                      'Prev',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: page > 0 ? const Color(0xff154670) : const Color(0xff154670).withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: page < (fishCount / 20).floor()
+                        ? () {
+                            setState(() {
+                              page++;
+                              getFishData();
+                              // print(_scrollController.position);
+                            });
+                            print(page);
+                            _scrollToTop();
+                          }
+                        : null,
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: page < (fishCount / 20).floor() ? const Color(0xff154670) : const Color(0xff154670).withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
